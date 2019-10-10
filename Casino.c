67ad37bb6ab4b8cb2a367a384f96947016942962
Palatio93 <51346771+Palatio93@users.noreply.github.com>
2019-10-10 09:30:24 -0500

    Colored and keep playing
    UI was improved, plus now you can keep playing several rounds of games and the current money is saved.

--- Casino.c
+++ Casino.c
@@ -6,22 +6,93 @@ int money = 100;
 void flip_coin(int, int);
 void Cho_Han(int, int);
 void High_Card(int);
+void Roulette(int, int);
 
-int main(){ // Games
-  int Guess = 0, Bet = 0;
-  printf("Let's play a game.\n");
-//  printf("Heads is 1 and Tails is 2.\n");
-//  printf("Sum odd is 1\t Sum even is 2\n");
-  printf("Game is pick a random card with values between 1 and 13.\n");
-  printf("Place your bet please.\n");
-  scanf("%d", &Bet);
-  printf("Give me your guess please.\n");
-  scanf("%d", &Guess);
-  //flip_coin(Guess, Bet);
-  //Cho_Han(Guess, Bet);
-  High_Card(Bet);
+int main(void){ // Games
+  int Guess = 0, Bet = 0, Player_Choice = 0, Game_of_choice = 1, keep_playing_casino = 0, keep_playing_game = 1;
+  printf("\033[1;36m"); // Color
+  printf("You wanna play in the casino?\n 1 for Yes \t Any key for No\n");
+  scanf("%d", &Player_Choice);
+  if (Player_Choice == 1){
+    do {
+      printf("What would you like to play?\n 1. Flip a coin \n 2. Cho Han\n 3. High Card\n 4. Roulette\n");
+      scanf("%d", &Game_of_choice);
+      printf("\033[0m");
+      switch (Game_of_choice) {
+        case 1: // Flip Coin
+          do {
+            printf("\033[01;33m");  // Color
+            printf("Heads is 1 and Tails is 2.\n");
+            printf("Place your bet please.\n");
+            scanf("%d", &Bet);
+            printf("Give me your guess please.\n");
+            scanf("%d", &Guess);
+            flip_coin(Guess, Bet);
+            printf("\033[01;33m");  // Color
+            printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
+            scanf("%d", &keep_playing_game);
+            printf("\033[0m");
+          } while(keep_playing_game == 1);
+        break;
+        case 2: // Cho Han
+          do {
+            printf("\033[1;31m"); // Color
+            printf("Sum odd is 1\t Sum even is 2\n");
+            printf("Place your bet please.\n");
+            scanf("%d", &Bet);
+            printf("Give me your guess please.\n");
+            scanf("%d", &Guess);
+            Cho_Han(Guess, Bet);
+            printf("\033[1;31m"); // Color
+            printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
+            scanf("%d", &keep_playing_game);
+            printf("\033[0m");
+          } while(keep_playing_game == 1);
+        break;
+        case 3: //  High Card
+          do {
+            printf("\033[0;34m"); // Color
+            printf("Game is pick a random card with values between 1 and 13 from the deck.\n");
+            printf("Place your bet please.\n");
+            scanf("%d", &Bet);
+            High_Card(Bet);
+            printf("\033[0;34m"); // Color
+            printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
+            scanf("%d", &keep_playing_game);
+            printf("\033[0m");
+          } while(keep_playing_game == 1);
+        break;
+        case 4: // Roulette
+          do {
+            printf("\033[0;35m"); // Color
+            printf("Place your bet please.\n");
+            scanf("%d", &Bet);
+            printf("Give me your guess please.\n");
+            scanf("%d", &Guess);
+            Roulette(Guess, Bet);
+            printf("\033[0;35m"); // Color
+            printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
+            scanf("%d", &keep_playing_game);
+            printf("\033[0m");
+          } while(keep_playing_game == 1);
+        break;
+      }
+      printf("\033[1;36m"); // Color
+      printf("\nYou want to stay in the casino?\n 1 for Yes \t Any key for No\n");
+      scanf("%d", &keep_playing_casino);
+    } while (keep_playing_casino == 1);
+  } else
+      printf("\033[1;36m"); // Color
+      printf("\nHave a nice day.\n");
+  if (keep_playing_casino != 1){
+    system("clear");
+    printf("\033[1;36m"); // Color
+    printf("Thank you for playing, see you next time.\n");
+  }
   return 0;
 }
+
+
 // Creating Flip Coin game
 void flip_coin(int guess, int bet) {
   srand(time(NULL));
@@ -29,24 +100,26 @@ void flip_coin(int guess, int bet) {
     int result = rand()%2+1; // Heads will be 1, Tails will be 2
     if (guess >= 1 && guess <= 2){
       if (guess == 1)
-        printf("You're rolling for Heads.\n");
+        printf("\nYou're rolling for Heads.\n");
       else
-        printf("You're rolling for Tails.\n");
+        printf("\nYou're rolling for Tails.\n");
       if (result == 1)
         printf("The coin fell on Heads.\n");
       else
         printf("The coin fell on Tails.\n");
       if ((guess == 1 && result == 1) || (guess == 2 && result == 2)){
         money += bet;
+        printf("\033[1;32m");
         printf("You won.\n Your new balance is %d.\n", money);
+        printf("\033[0m");
       } else{
         money -= bet;
         printf("You lost.\n Your new balance is %d.\n", money);
       }
     } else
-      printf("Not a valid guess.\n");
+      printf("\nNot a valid guess.\n");
   } else
-    printf("Bet not valid.\n");
+    printf("\nBet not valid.\n");
 }
 
 // Creating Cho Han game
@@ -56,26 +129,28 @@ void Cho_Han(int guess, int bet) { // Sum even is 2, sum odd is 1
     int Dice1 = rand()%6+1, Dice2 = rand()%6+1;
     if (guess >= 1 && guess <= 2){
       if (guess == 1)
-        printf("You're going for sum odd.\n");
+        printf("\nYou're going for sum odd.\n");
       else
-        printf("You're going for sum even.\n");
+        printf("\nYou're going for sum even.\n");
       if ((Dice1 + Dice2) % 2 == 0)
         printf("The sum is even.\n");
       else
         printf("The sum is odd.\n");
       if (((Dice1 + Dice2) % 2 == 0 && (guess == 2)) || ((Dice1 + Dice2) % 2 != 0 && (guess == 1))) {
+        printf("\033[1;32m"); //Color
         printf("You won!.\n");
         money += bet;
         printf("New balance is: %d\n", money);
+        printf("\033[0m");
       } else {
         printf("You lost.\n");
         money -= bet;
         printf("New balances is: %d\n", money);
       }
     } else
-      printf("Not a valid guess.\n");
+      printf("\nNot a valid guess.\n");
   } else
-    printf("Bet not valid.\n");
+    printf("\nBet not valid.\n");
 }
 
 //Creating High Card game
@@ -85,22 +160,30 @@ void High_Card(int bet) {
           srand(time(NULL));
           int Result = rand()%13+1;  // House pick
           int Player_Result = rand()%13+1;   //Player pick
-          printf("You picked %d.\n", Player_Result);
-          printf("House picked %d.\n", Result);
+          printf("\nYou picked %d.\n", Player_Result);
+          printf("\nHouse picked %d.\n", Result);
           if (Player_Result > Result) {    // Player wins
+              printf("\033[1;32m"); //Color
               printf("You won!\n");
               money += bet;
               printf("New balance is %d.\n", money);
+              printf("\033[0m");
           } else if (Player_Result < Result) { // Player loses
               printf("You lost.\n");
               money -= bet;
               printf("New balance is %d.\n", money);
           } else {
-              printf("That's a tie!");
+              printf("\033[0;32m");
+              printf("\nThat's a tie!\n");
               printf("New balance is %d.\n", money);
+              printf("\033[0m");
           }
       } else
-          printf("You don't have enough money to play.\n");
+          printf("\nYou don't have enough money to play.\n");
   } else
-      printf("You don't have enough money to play.\n");
+      printf("\nYou don't have enough money to play.\n");
+}
+
+void Roulette(int guess, int bet) {
+
 }
