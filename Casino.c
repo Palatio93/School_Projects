57dbe1d41b4153aff67ea3f526fc74d814fe1f10
Palatio93 <51346771+Palatio93@users.noreply.github.com>
2019-10-12 17:24:25 -0500

    One step to finish
    All games are completed now, just check when runs out of money.

--- Casino.c
+++ Casino.c
@@ -6,7 +6,7 @@ int money = 100;
 void flip_coin(int, int);
 void Cho_Han(int, int);
 void High_Card(int);
-void Roulette(int, int);
+void Roulette(int);
 
 int main(void){ // Games
   int Guess = 0, Bet = 0, Player_Choice = 0, Game_of_choice = 1, keep_playing_casino = 0, keep_playing_game = 1;
@@ -21,59 +21,46 @@ int main(void){ // Games
       switch (Game_of_choice) {
         case 1: // Flip Coin
           do {
-            printf("\033[01;33m");  // Color
             printf("Heads is 1 and Tails is 2.\n");
             printf("Place your bet please.\n");
             scanf("%d", &Bet);
             printf("Give me your guess please.\n");
             scanf("%d", &Guess);
             flip_coin(Guess, Bet);
-            printf("\033[01;33m");  // Color
             printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
             scanf("%d", &keep_playing_game);
-            printf("\033[0m");
           } while(keep_playing_game == 1);
         break;
         case 2: // Cho Han
           do {
-            printf("\033[1;31m"); // Color
             printf("Sum odd is 1\t Sum even is 2\n");
             printf("Place your bet please.\n");
             scanf("%d", &Bet);
             printf("Give me your guess please.\n");
             scanf("%d", &Guess);
             Cho_Han(Guess, Bet);
-            printf("\033[1;31m"); // Color
             printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
             scanf("%d", &keep_playing_game);
-            printf("\033[0m");
           } while(keep_playing_game == 1);
         break;
         case 3: //  High Card
           do {
-            printf("\033[0;34m"); // Color
             printf("Game is pick a random card with values between 1 and 13 from the deck.\n");
             printf("Place your bet please.\n");
             scanf("%d", &Bet);
             High_Card(Bet);
-            printf("\033[0;34m"); // Color
             printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
             scanf("%d", &keep_playing_game);
-            printf("\033[0m");
           } while(keep_playing_game == 1);
         break;
         case 4: // Roulette
-          do {
-            printf("\033[0;35m"); // Color
+          printf("Welcome to Roulette\n");
+            do {
             printf("Place your bet please.\n");
             scanf("%d", &Bet);
-            printf("Give me your guess please.\n");
-            scanf("%d", &Guess);
-            Roulette(Guess, Bet);
-            printf("\033[0;35m"); // Color
+            Roulette(Bet);
             printf("\nYou want to stay in this game?\n 1 for Yes \t Any key for No\n");
             scanf("%d", &keep_playing_game);
-            printf("\033[0m");
           } while(keep_playing_game == 1);
         break;
       }
@@ -184,6 +171,97 @@ void High_Card(int bet) {
       printf("\nYou don't have enough money to play.\n");
 }
 
-void Roulette(int guess, int bet) {
-
+// Creating Roulette game
+void Roulette(int bet) {
+  int Player_guess = 0, roulette_guess_type = 0;
+  int column1[12] = {1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34};
+  int column2[12] = {2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35};
+  int column3[12] = {3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36};
+  printf("  First column \t Second Column \t Third column\n");
+  for (int i=0; i<12; i++) {
+    printf("\t%d\t\t %d\t\t %d\n", column1[i], column2[i], column3[i]);
+  }
+  printf("You're betting for:\n 1. A number\n 2. Even\n 3. Odd\n 4. First column\n 5. Second column\n 6. Third column\n");
+  scanf("%d", &roulette_guess_type);
+  srand(time(NULL));
+  int Result = rand()%37;
+  switch (roulette_guess_type) {
+    case 1:
+      printf("What number between 0 and 36 you wanna bet?\n");
+      scanf("%d", &Player_guess);
+      printf("Ball fall in %d.\n", Result);
+      if (Player_guess == Result){
+        printf("You won.\n");
+        money += (bet * 36);  // Pays 36 to 1
+        printf("New balance is %d.\n", money);
+      } else {
+        printf("You lost.\n");
+        money -= bet;
+        printf("New balance is %d.\n", money);
+      }
+    break;
+    case 2:
+      printf("Ball fall in %d.\n", Result);
+      if (Result % 2 == 0){
+        printf("You won.\n");
+        money += (bet * 2); // Pays 2 to 1
+        printf("New balance is %d.\n", money);
+      } else {
+        printf("You lost.\n");
+        money -= bet;
+        printf("New balance is %d.\n", money);
+      }
+    break;
+    case 3:
+      printf("Ball fall in %d.\n", Result);
+      if (Result % 2 != 0){
+        printf("You won.\n");
+        money += (bet * 2); // Pays 2 to 1
+        printf("New balance is %d.\n", money);
+      } else {
+        printf("You lost.\n");
+        money -= bet;
+        printf("New balance is %d.\n", money);
+      }
+    break;
+    case 4:
+      printf("Ball fall in %d.\n", Result);
+      for(int i = 0; i<=12; i++){
+        if (Result == column1[i]){
+          printf("You won.\n");
+          money += (bet * 3); // Pays 3 to 1
+          printf("New balance is %d.\n", money);
+        }
+      }
+      printf("You lost.\n");
+      money -= bet;
+      printf("New balance is %d.\n", money);
+    break;
+    case 5:
+      printf("Ball fall in %d.\n", Result);
+      for(int i = 0; i<=12; i++){
+        if(Result == column2[i]){
+          printf("You won.\n");
+          money += (bet * 3); // Pays 3 to 1
+          printf("New balance is %d.\n", money);
+        }
+      }
+      printf("You lost.\n");
+      money -= bet;
+      printf("New balance is %d.\n", money);
+    break;
+    case 6:
+      printf("Ball fall in %d.\n", Result);
+      for(int i = 0; i<=12; i++){
+        if(Result == column3[i]){
+          printf("You won.\n");
+          money += (bet * 3); // Pays 3 to 1
+          printf("New balance is %d.\n", money);
+        }
+      }
+      printf("You lost.\n");
+      money -= bet;
+      printf("New balance is %d.\n", money);
+    break;
+  }
 }
